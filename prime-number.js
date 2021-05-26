7871a3a740e6e2807111979cae39176067c964d0
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-05-26 13:04:23 +0530

    feat

--- prime-number.js
+++ prime-number.js
@@ -1,1 +1,28 @@
-var num = prompt("Enter your date of birth")
+var userName = prompt("Enter your name");
+
+var output1 = document.querySelector(".display-output1");
+
+output1.innerText = "Hi "+ userName +", Let's see if your birth date is a prime number!";
+
+var num=0;
+
+setTimeout(function(){
+    num = prompt("Tell me the date you were born.");
+}, 3000);
+
+var output2 = document.querySelector(".display-output2"); 
+
+var count = 0;
+
+for(var i=1; i<num; i++){
+  if(num%i == 0){
+    count++;
+  }
+}
+
+if(count == 1){
+  output2.innerText = "Your birth date is a prime number!!!"
+}
+else{
+    output2.innerText = "Nope! Definetly not a prime number!"
+}
